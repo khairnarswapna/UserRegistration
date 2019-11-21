d7c4b0cffdff820b943ec3efa163848ef36b841b
khairnarswapna <khairnarswapna99@gmail.com>
2019-11-21 12:27:18 +0530

    validation of user First name

--- userReg.sh
+++ userReg.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo  ********Welcome to UserRegistration*****
  
+echo "Please Enter the FirstName" 
+read firstName
+firstNameRegex="^[A-Z]([A-Za-z]{3,})$"
+if [[ $firstName =~ $firstNameRegex ]]
+then
+	echo "valid firstName"
+else
+	echo "invalid firstname"
+fi
+
