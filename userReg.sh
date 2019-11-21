568460ccd995945ea63c77e6bccab27cc6211f4c
khairnarswapna <khairnarswapna99@gmail.com>
2019-11-21 18:32:11 +0530

    validation of password should have atleast one Uppercase

--- userReg.sh
+++ userReg.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash -x
 
 echo  ********Welcome to UserRegistration*****
@@ -5,7 +6,11 @@ echo  ********Welcome to UserRegistration*****
 userNamePattern="^[A-Z]+[a-z]{3,}$"
 EmailPattern="^[a-zA-Z0-9]([._+]{0,1}[a-zA-Z0-9])*[@]{1}[a-zA-Z]{1,}[.]{1}[a-zA-Z]{2,3}([.]{1}[a-zA-Z]{2}){0,1}$"
 mobileFormat="^[0-9]{2,3}[: :][0-9]{10}$"
-passwordPattern="^[a-zA-Z0-9]{8,}$"
+
+#passwordPattern2="^[a-zA-Z0-9]*(.*[A-Z].*{1,}+)[a-zA-Z0-9]*{8,}$"
+
+passwordPattern2="[a-z]*[A-Z]+[a-z]*{8}"
+
 function validationOfFirstName()
 { 
 	echo "Please Enter the FirstName" 
@@ -39,9 +44,9 @@ function validationOfEmail()
 	echo "please Enter the Email"
 	read email
 	if [[ $email =~ $EmailPattern ]]
-   then
-   	echo "valid Email"
-   else
+   	then
+   		echo "valid Email"
+   	else
    	echo "invalid Email"
    validationOfEmail
    fi
@@ -62,9 +67,9 @@ function mobileFormat()
 }
 function validationOfPassword()
 {
-	echo "please Enter the Password"
+   echo "please Enter the Password"
    read password
-   if [[ $password =~ $passwordPattern ]]
+   if [[ $password =~ $passwordPattern2 ]]
    then
    	echo "valid password"
    else
