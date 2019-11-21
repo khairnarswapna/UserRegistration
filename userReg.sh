951fb695a846b8ab4676f382c5784add709580ed
khairnarswapna <khairnarswapna99@gmail.com>
2019-11-21 13:03:49 +0530

    validation of user last name

--- userReg.sh
+++ userReg.sh
@@ -1,14 +1,36 @@
 #!/bin/bash -x
 
 echo  ********Welcome to UserRegistration*****
- 
-echo "Please Enter the FirstName" 
-read firstName
-firstNameRegex="^[A-Z]([A-Za-z]{3,})$"
-if [[ $firstName =~ $firstNameRegex ]]
-then
+
+userNamePattern="^[A-Z]+[a-z]{3,}$"
+function ValidionOfFirstName()
+{ 
+	echo "Please Enter the FirstName" 
+	read firstName
+	if [[ $firstName =~ $userNamePattern ]]
+	then
 	echo "valid firstName"
-else
+	else
 	echo "invalid firstname"
-fi
+	validationOfLastName
+	fi
+
+}
+function validationOfLastName()
+{
+	echo "Please Enter the lastName" 
+   read lastName
+   if [[ $lastName =~ $userNamePattern ]]
+   then
+   echo "valid lastName"
+   else
+   echo "invalid lasttname"
+	ValidionOfFirstName
+   fi
+}
+ValidionOfFirstName
+validationOfLastName
+
+
+
 
